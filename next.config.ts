8c228767b8e38f491a8991c4ed2261953e260340
Vansh Mishra <164024648+vansh-15@users.noreply.github.com>
2026-09-13 20:37:39 +0530

    ch1

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
+  basePath: "/law-firm-website",
+  assetPrefix: "/law-firm-website/",
   images: {
     unoptimized: true,
   },
